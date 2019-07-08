3bf990187611165d55d85755543c405a3ce594d5
HannahKhuc <hannahkhuc@gmail.com>
2019-07-08 14:52:46 -0700

    update files

---
+++ public/js/setup_firebase.js
@@ -0,0 +1,14 @@
+<script src="https://www.gstatic.com/firebasejs/5.8.5/firebase.js"></script>
+<script>
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAhB-Kx2-NBGGTSMhVzZUZ2-1dDzbMvuIk",
+    authDomain: "maxbuddee.firebaseapp.com",
+    databaseURL: "https://maxbuddee.firebaseio.com",
+    projectId: "maxbuddee",
+    storageBucket: "",
+    messagingSenderId: "544611626826",
+    appId: "1:544611626826:web:1eba22614246f6aa"
+};
+firebase.initializeApp(config);    
+</script>
